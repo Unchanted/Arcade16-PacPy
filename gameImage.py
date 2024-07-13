754a4349571b5dc95c98b64d5a6c19ebbdb2da64
Unchanted <omkkdwivedi@gmail.com>
2024-07-13 14:00:18 +0530

    final commit

--- gameImage.py
+++ gameImage.py
@@ -1,4 +1,6 @@
 from PIL.ImageTk import PhotoImage
+from pathlib import Path 
+
 import os
 
 class GameImage():
@@ -9,11 +11,11 @@ class GameImage():
             through all of the images inside the 'images' directory. This makes all of the
             images available from different directories. '''
         self.start_directory = os.getcwd()
-        self.image_directory = f'{self.start_directory}\\images'
+        self.image_directory = Path('.')/"images"
         self.game_images = dict()
     
-        for image in os.listdir( self.image_directory ):
-            self.game_images[ image[:-4] ] = PhotoImage(file= f'{self.image_directory}\\{image}')
+        for image in self.image_directory.iterdir():
+            self.game_images[ image.name.split('.')[0] ] = PhotoImage(file=image)
 
     def return_image(self, image) -> PhotoImage:
         ''' Returns the image from the image_directory given the image argument. '''
